6c7aac6f8b591d12ef2ea361da6356939eb589e3
Claas <claas.moehlmann@outlook.com>
2023-03-22 17:19:28 +0100

    Add abstract elmish component class

--- src/counter.ts
+++ src/counter.ts
@@ -1,9 +1,16 @@
 export function setupCounter(element: HTMLButtonElement) {
-  let counter = 0
+  let counter = 0;
   const setCounter = (count: number) => {
-    counter = count
-    element.innerHTML = `count is ${counter}`
-  }
-  element.addEventListener('click', () => setCounter(counter + 1))
-  setCounter(0)
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", () => setCounter(counter + 1));
+  setCounter(0);
+}
+
+type Dispatch<TMessage> = (message: TMessage) => void;
+abstract class ElmishComponent<TModel, TMessage> extends HTMLElement {
+  abstract update(model: TModel, message: TMessage): TModel;
+
+  abstract view(model: TModel, dispatch: Dispatch<TMessage>): string;
 }
