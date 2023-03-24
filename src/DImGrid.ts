f9bd26a3c3d9d387c48790c9d8fd75818f891142
Claas <claas.moehlmann@outlook.com>
2023-03-25 00:59:39 +0100

    Add calculation of column width

--- src/DImGrid.ts
+++ src/DImGrid.ts
@@ -13,6 +13,7 @@
  * We calculate the ratio of each element by counting the tokens width and height starting on the top left
  * The element will create slots based on how many different tokens there are. The slots name will be their token.
  * It is an excerise in building web components.
+ * Find source where I have the idea from with defining the grid with letters
  */
 
 function createSlots(distinctTokens: Set<string>) {
@@ -24,16 +25,26 @@ function createSlots(distinctTokens: Set<string>) {
   });
 }
 
+const css = `
+  div {
+    display: grid;
+    grid-template: "a b" "c d";
+    background-color: green;
+  }
+
+  ::slotted(*) {
+    background-color: red;
+  }
+`;
 export default class DimGrid extends HTMLElement {
   constructor() {
     super();
 
-    const shadowRoot = this.attachShadow({ mode: "closed" });
-    const textElement = document.createElement("span");
+    const shadowRoot = this.attachShadow({ mode: "open" });
 
-    const templateAttribute = this.attributes.getNamedItem("template");
+    const templateAttribute = this.getAttribute("template");
     if (templateAttribute) {
-      const templateString = templateAttribute.value;
+      const templateString = templateAttribute;
       const gridLayout = templateString
         // Trim so we don't end up with empty lines
         .trim()
@@ -46,15 +57,50 @@ export default class DimGrid extends HTMLElement {
         gridLayout.map((string) => [...string]).flat()
       );
 
+      // Calculate ratio
+      // Get distinct tokens in first row
+      for (const row of gridLayout) {
+        const widthByToken = new Map<string, number>();
+
+        let previousToken = row[0];
+        let tokenWidth = 1;
+        for (const currentToken of row.slice(1)) {
+          if (previousToken === currentToken) {
+            // Case continuation of token
+            tokenWidth++;
+            continue;
+          }
+
+          // Case switch to new token
+          // Save width and continue to next token
+          widthByToken.set(previousToken, tokenWidth);
+          // Reset to one
+          tokenWidth = 1;
+          // Switch
+          previousToken = currentToken;
+        }
+
+        // Add final token
+        widthByToken.set(previousToken, tokenWidth);
+
+        console.debug(row, widthByToken);
+      }
+
+      // Add styles. Is there a better way?
+      const style = document.createElement("style");
+
+      style.innerHTML = css;
+      shadowRoot.appendChild(style);
+
+      const wrapper = document.createElement("div");
+
+      shadowRoot.appendChild(wrapper);
+
+      // Add token slots
       const tokenSlots = createSlots(distinctTokens);
-      shadowRoot.append(...tokenSlots);
 
-      console.log("Distinct tokens", distinctTokens);
-      console.log(templateString);
-      textElement.textContent = templateString;
+      wrapper.append(...tokenSlots);
     }
-
-    shadowRoot.appendChild(textElement);
   }
 }
 
---
+++ src/centerDiv.ts
@@ -0,0 +1,5 @@
+//
+/**
+ * Properly centers a div. (This is a meme component. It does not center a div because it is too hard)
+ */
+export class CenterDiv extends HTMLElement {}
