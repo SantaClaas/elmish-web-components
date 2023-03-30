8f3c7cf5ac9f1862b859d618b990f128a0e5cac8
Claas <claas.moehlmann@outlook.com>
2023-03-30 21:56:39 +0200

    Port non fable version of weak output set using weak references

--- src/adaptive/byReference.ts
+++ src/adaptive/byReference.ts
@@ -1,3 +1,6 @@
 // Just a mock for ref<T> for completeness and not deviate from original source but probably not necessary
 // Since this is of type object it will be passed by reference in JS and the value can be a value type but we access it through this object so it will behave like a reference type if accessed through this objectdi
 export type ByReference<T> = { value: T };
+export function toReference<T>(value: T): ByReference<T> {
+  return { value };
+}
--- src/adaptive/core/core.ts
+++ src/adaptive/core/core.ts
@@ -1,6 +1,7 @@
-import { ByReference } from "../byReference";
+import { ByReference, toReference } from "../byReference";
 import { createReferenceHashSet, ReferenceHashSet } from "../equality";
 import { resizeArray } from "../fableHelpers";
+import { IterableWeakSet } from "./iterableWeakSet";
 
 //TODO sort out the ref situation since we expect everything of IAdaptiveObject to be ref types anyways don't we?
 
@@ -16,7 +17,7 @@ export interface IAdaptiveObject {
    * Each object can cache a WeakReference pointing to itself.
    * This is because the system internally needs WeakReferences to IAdaptiveObjects
    */
-  weak: WeakRef<ByReference<IAdaptiveObject>>;
+  weak: WeakRef<IAdaptiveObject>;
   /**
    * Used internally to represent the maximal distance from an input
    * cell in the dependency graph when evaluating inside a transaction.
@@ -300,22 +301,27 @@ export class FableWeakOutputSet implements IWeakOutputSet {
  * We need to test if this is the case in JS too
  */
 type VolatileSetData = {
-  single: WeakRef<ByReference<IAdaptiveObject>>;
-  array: WeakRef<ByReference<IAdaptiveObject>>[];
+  single: WeakRef<IAdaptiveObject> | null;
+  array: (WeakRef<IAdaptiveObject> | null)[];
   // I think this is the same as Set<WeakRef<IAdaptableObject>> in contrast to source but I am not sure
-  set: WeakSet<ByReference<IAdaptiveObject>>;
+  set: IterableWeakSet<IAdaptiveObject>;
   tag: number;
 };
 
-export class WeakOutputSet {
-  #data: VolatileSetData | null = null;
-  #setOperations: number = 0;
-  #valueReader: ByReference<IAdaptiveObject | null> | undefined = {
-    value: null,
+export class WeakOutputSet implements IWeakOutputSet {
+  // Yes, I know
+  #data: VolatileSetData = {
+    single: null,
+    array: null!,
+    set: null!,
+    tag: 0,
   };
+  #setOperationsCount = 0;
+  // Don't need the set operations count because we don't require a clean up function (see why below)
+  #valueReader: IAdaptiveObject | null | undefined = null;
 
-  add(object: ByReference<IAdaptiveObject>): boolean {
-    const weakObject = object.value.weak;
+  #add(object: IAdaptiveObject): boolean {
+    const weakObject = object.weak;
     switch (this.#data?.tag) {
       case 0:
         if (this.#data.single === null) {
@@ -328,12 +334,10 @@ export class WeakOutputSet {
         this.#valueReader = this.#data.single.deref();
         if (this.#valueReader !== undefined) {
           const isFound = this.#valueReader === object;
-          this.#valueReader.value = null;
+          this.#valueReader = null;
           if (isFound) return false;
 
-          const array = Array.from<WeakRef<ByReference<IAdaptiveObject>>>(
-            Array(8)
-          );
+          const array = Array(8).fill(null);
 
           // Switch to array as we have two objects
           array[0] = this.#data.single;
@@ -359,7 +363,7 @@ export class WeakOutputSet {
             freeIndex = -2;
             index = length;
           } else {
-            this.#valueReader = this.#data.array[index].deref();
+            this.#valueReader = this.#data.array[index]!.deref();
             if (this.#valueReader !== undefined) {
               if (this.#valueReader === object) {
                 freeIndex = -2;
@@ -374,36 +378,211 @@ export class WeakOutputSet {
           index++;
         }
 
-        let result;
+        let result: boolean;
         if (freeIndex === -2) {
           result = false;
         } else if (freeIndex >= 0) {
           this.#data.array[freeIndex] = weakObject;
           result = true;
         } else {
-          const all = this.#data.array
-            // Need to tell TS it is not undefined here but we check later
-            .map((reference) => reference.deref()!)
-            .filter((reference) => reference !== undefined);
-          const set = new WeakSet(all);
+          // Need cast because TS doesn't understand they are not null after the filter
+          // Also remove dead references
+          const all = this.#data.array.filter(
+            (element) => element !== null && element.deref() !== undefined
+          ) as WeakRef<IAdaptiveObject>[];
+          const set = new IterableWeakSet<IAdaptiveObject>(all);
 
-          result = set.has(object);
-          // Original source adds weakObject but it uses HashSet<WeakREference> and we use WeakSet
-          set.add(object);
+          result = set.add(weakObject);
+
           this.#data.tag = 2;
           this.#data.set = set;
         }
 
         // Not sure here either. Original sets to defaultof
-        this.#valueReader = { value: null };
+        this.#valueReader = null;
         return result;
       default:
-        if (this.#data?.set.has(object)) {
-          return false;
+        return this.#data.set.add(weakObject);
+    }
+  }
+
+  // The only reason we have to use a manual WeakSet implementation is because the native one
+  // is not iterable...
+  /**
+   * Used internally to get rid of leaking WeakReferences
+   */
+  #cleanup() {
+    if (this.#setOperationsCount <= 100) return;
+
+    // Our port is not concurrency safe. So if this is shared between a worker and the main thread it should be locked.
+    this.#setOperationsCount = 0;
+    const array: IAdaptiveObject[] = Array(100).fill(undefined);
+    const count = this.consume(toReference(array));
+    for (let index = 0; index < count; index++) this.#add(array[index]);
+  }
+
+  /**
+   * Adds a weak reference to the given AdaptiveObject to the set
+   * And returns a boolean indicating whether the obj was new.
+   */
+  add(object: IAdaptiveObject): boolean {
+    if (object.isConstant) return false;
+
+    if (!this.#add(object)) return false;
+
+    this.#setOperationsCount++;
+    this.#cleanup();
+    return true;
+  }
+
+  /**
+   * Removes the reference to the given AdaptiveObject from the set
+   * And returns a boolean indicating whether the obj was removed.
+   */
+  remove(object: IAdaptiveObject): boolean {
+    if (object.isConstant) return false;
+
+    let old: IAdaptiveObject | undefined;
+
+    switch (this.#data?.tag) {
+      case 0:
+        if (this.#data.single === null) return false;
+
+        old = this.#data.single.deref();
+        if (old === undefined) return false;
+
+        this.#data.single = null;
+        return true;
+      case 1:
+        let isFound = false;
+        const length = this.#data.array.length;
+
+        let count = 0;
+        let living = null;
+
+        for (let index = 0; index < length; index++) {
+          const reference = this.#data.array[index];
+          if (reference === null) continue;
+
+          old = reference.deref();
+
+          // Dead reference
+          if (old === undefined) {
+            // Clean up
+            this.#data.array[index] = null;
+            continue;
+          }
+
+          if (old !== object) {
+            count++;
+            living = reference;
+            continue;
+          }
+
+          // Yay, found it
+          this.#data.array[index] = null;
+          isFound = true;
         }
 
-        this.#data?.set.add(object);
+        if (count === 0) {
+          // Switch to single
+          this.#data.tag = 0;
+          this.#data.single = null;
+          return isFound;
+        }
+
+        if (count === 1) {
+          // Switch to single
+          this.#data.tag = 0;
+          this.#data.single = living;
+          return isFound;
+        }
+
+        // Here we have count > 2 so we stay in array mode
+        return isFound;
+
+      default:
+        if (!this.#data!.set.remove(object.weak)) return false;
+
+        this.#setOperationsCount++;
+        this.#cleanup();
         return true;
+    }
+  }
+  consume(output: ByReference<IAdaptiveObject[]>): number {
+    switch (this.#data.tag) {
+      case 0:
+        if (this.#data.single === null) return 0;
+
+        const value = this.#data.single.deref();
+        if (value === undefined) {
+          // Original source doesn't clean up the reference here but shouldn't we?
+          this.#data.single = null;
+          return 0;
+        }
+
+        output.value[0] = value;
+        this.clear();
+        return 1;
+      case 1: {
+        let outputIndex = 0;
+        const array = this.#data.array;
+        for (let index = 0; index < array.length; index++) {
+          const reference = array[index];
+          if (reference === null) continue;
+
+          const value = reference.deref();
+          if (value === undefined) {
+            // Again, original source doesn't clean up the reference here but shouldn't we?
+            array[index] = null;
+            continue;
+          }
+
+          if (outputIndex >= output.value.length)
+            resizeArray(output, outputIndex << 2);
+
+          output.value[outputIndex] = value;
+          outputIndex++;
+        }
+
+        this.clear();
+        return outputIndex;
+      }
+
+      default:
+        let outputIndex = 0;
+        for (const reference of this.#data.set.values()) {
+          const object = reference.deref();
+          if (object === undefined) continue;
+          // Resize if too large
+          if (outputIndex >= output.value.length)
+            resizeArray(output, outputIndex << 2);
+
+          output.value[outputIndex] = object;
+          outputIndex++;
+        }
+
+        this.clear();
+        return outputIndex;
+    }
+  }
+
+  clear() {
+    this.#data!.single = null;
+    this.#data!.tag = 0;
+    this.#setOperationsCount = 0;
+  }
+
+  /**
+   * Indicates whether the set is (conservatively) known to be empty.
+   * Note that we don't dereference any WeakReferences here.
+   */
+  get isEmpty() {
+    switch (this.#data!.tag) {
+      case 0:
+        return this.#data!.single === null;
+      default:
+        return false;
     }
   }
 }
