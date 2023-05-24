68516e30aabe7982c1cdb1aacd253dfcb822a764
Claas <claas.moehlmann@outlook.com>
2023-05-24 15:35:45 -0600

    Fix development load of service worker
    due to vite inserting an import statement and the script not being loaded as module

--- examples/dim-ice-app/registerServiceWorker.ts
+++ examples/dim-ice-app/registerServiceWorker.ts
@@ -1,4 +1,4 @@
 import serviceWorker from "./service worker/serviceworker?worker&url";
 
 if ("serviceWorker" in navigator)
-  navigator.serviceWorker.register(serviceWorker);
+  navigator.serviceWorker.register(serviceWorker, { type: "module" });
